a568e8c291532b0dff0b0719892f887d28a6ce72
Telegenic <martinguajardo20@gmail.com>
2019-09-30 15:23:16 -0500

    Se agregaron 2 form, telefono y correo

--- Practica8/src/app/FrmMyApp.java
+++ Practica8/src/app/FrmMyApp.java
@@ -17,23 +17,39 @@ public class FrmMyApp extends JFrame{
 
         //Name form
         JLabel LblName = new JLabel("Nombre:");
-        LblName.setBounds(25,25,100,20);
-        JTextField TxtName = new JTextField();
-        TxtName.setBounds(100,25,100,20);
+        LblName.setBounds(25,30,100,20);
         add(LblName);
+        JTextField TxtName = new JTextField();
+        TxtName.setBounds(100,30,100,20);
         add(TxtName);
         
         //Last name form
         JLabel LblLastName = new JLabel("Apellido:");
         LblLastName.setBounds(25,60,100,20);
+        add(LblLastName);
         JTextField TxtLastName = new JTextField();
         TxtLastName.setBounds(100,60,100,20);
-        add(LblLastName);
         add(TxtLastName);
+
+        //E-Mail form
+        JLabel LblMail = new JLabel("E-Mail:");
+        LblMail.setBounds(25,90,100,20);
+        add(LblMail);
+        JTextField TxtMail = new JTextField();
+        TxtMail.setBounds(100,90,100,20);
+        add(TxtMail);
+
+        //Telephone form
+        JLabel LblPhone = new JLabel("Telefono:");
+        LblPhone.setBounds(25,120,100,20);
+        add(LblPhone);
+        JTextField TxtPhone = new JTextField();
+        TxtPhone.setBounds(100,120,100,20);
+        add(TxtPhone);
 
         //Accept button
         JButton BtnAcept = new JButton("Click");
-        BtnAcept.setBounds(25,100,100,100);
+        BtnAcept.setBounds(200,30,75,75);
         add(BtnAcept);
 
         //Frame Properties
