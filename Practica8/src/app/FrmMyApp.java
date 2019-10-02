194c1d6ecfebee2bc849366d8d294fa2fa1dedfc
Telegenic <martinguajardo20@gmail.com>
2019-10-02 15:44:53 -0500

    Se agregaron los botones siguiente y anterior y se empezo el uso de constantes

--- Practica8/src/app/FrmMyApp.java
+++ Practica8/src/app/FrmMyApp.java
@@ -15,42 +15,69 @@ public class FrmMyApp extends JFrame{
 
     public void init() {
 
+        //Places properties
+        int[] rows = {30,65,100,135};
+        int[] columns = {25,100,210};
+
+        //Label & Text properties
+        int infoWidthP = 100; //Width of Labels & Texts
+        int infoHeightP = 20;  //Height of Labels & Texts
+
         //Name form
         JLabel LblName = new JLabel("Nombre:");
-        LblName.setBounds(25,30,100,20);
+        LblName.setBounds(columns[0],rows[0],infoWidthP,infoHeightP);
         add(LblName);
         JTextField TxtName = new JTextField();
-        TxtName.setBounds(100,30,100,20);
+        TxtName.setBounds(columns[1],rows[0],infoWidthP,infoHeightP);
         add(TxtName);
         
         //Last name form
         JLabel LblLastName = new JLabel("Apellido:");
-        LblLastName.setBounds(25,60,100,20);
+        LblLastName.setBounds(columns[0],rows[1],infoWidthP,infoHeightP);
         add(LblLastName);
         JTextField TxtLastName = new JTextField();
-        TxtLastName.setBounds(100,60,100,20);
+        TxtLastName.setBounds(columns[1],rows[1],infoWidthP,infoHeightP);
         add(TxtLastName);
 
         //E-Mail form
         JLabel LblMail = new JLabel("E-Mail:");
-        LblMail.setBounds(25,90,100,20);
+        LblMail.setBounds(columns[0],rows[2],infoWidthP,infoHeightP);
         add(LblMail);
         JTextField TxtMail = new JTextField();
-        TxtMail.setBounds(100,90,100,20);
+        TxtMail.setBounds(columns[1],rows[2],infoWidthP,infoHeightP);
         add(TxtMail);
 
         //Telephone form
         JLabel LblPhone = new JLabel("Telefono:");
-        LblPhone.setBounds(25,120,100,20);
+        LblPhone.setBounds(columns[0],rows[3],infoWidthP,infoHeightP);
         add(LblPhone);
         JTextField TxtPhone = new JTextField();
-        TxtPhone.setBounds(100,120,100,20);
+        TxtPhone.setBounds(columns[1],rows[3],infoWidthP,infoHeightP);
         add(TxtPhone);
 
+        //Buttons Properties
+        int btnHeightP = 30; //Height of buttons
+        int btnWidthP = 125; //Width of buttons
+        
+        //Clear button
+        JButton BtnClear = new JButton("Borrar");
+        BtnClear.setBounds(columns[2],rows[0],btnWidthP,btnHeightP);
+        add(BtnClear);
+
         //Accept button
-        JButton BtnAcept = new JButton("Click");
-        BtnAcept.setBounds(200,30,75,75);
-        add(BtnAcept);
+        JButton BtnAccept = new JButton("Aceptar");
+        BtnAccept.setBounds(columns[2],rows[1],btnWidthP,btnHeightP);
+        add(BtnAccept);
+
+        //Next button
+        JButton BtnNext = new JButton("Siguiente");
+        BtnNext.setBounds(columns[2],rows[2],btnWidthP,btnHeightP);
+        add(BtnNext);
+
+        //Previous button
+        JButton BtnPrevious = new JButton("Anterior");
+        BtnPrevious.setBounds(columns[2],rows[3],btnWidthP,btnHeightP);
+        add(BtnPrevious);
 
         //Frame Properties
         setSize(500,500);
