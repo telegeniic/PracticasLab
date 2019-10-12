3f7f60bb0bafa26945f47a4fa11397f40f149394
Telegenic <martinguajardo20@gmail.com>
2019-10-12 08:28:48 -0500

    Se termino la practica 10

--- Practica10/src/app/App.java
+++ Practica10/src/app/App.java
@@ -1,5 +1,9 @@
 package app;
 
+/*
+clase para agregar datos que en el arreglo
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
         Man alberto = new Man();
@@ -10,9 +14,26 @@ public class App {
 
         for (int j = 0; j < 5; j++) {
             juanes[j] = new Man();
-            juanes[j].setNombre("carlos" + j);
-            juanes[j].setEdad("23");
-            System.out.println(juanes[j].getEdad());
+            juanes[j].setNombre("carlos " + Integer.toString(j));
+            juanes[j].setEdad("23 " + Integer.toString(j));
         }
+
+        //Man[] r = ImprimeArreglo(juanes);
+        ImpArreglo(ImprimeArreglo(juanes));
+    }
+
+    public static Man[] ImprimeArreglo(Man[] m) {
+        return m;
+    }
+
+    public static void ImpArreglo(Man[] m) {
+        for (Man n : m) {
+            String msj = n.getNombre() + " " + n.getEdad();
+            Imprimir(msj);
+        }
+    }
+
+    public static void Imprimir(Object imp){
+        System.out.println(imp);
     }
 }
